478032aee1c7c508b8811e4af81be9dd21c4e783
Hugo S. Dias <hugo_sdias@hotmail.com>
2022-10-15 10:51:18 -0300

    resolução desáfio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone("9999-6415", "13 PRO", "5468761685681681", 128);
+Nokia nokia = new Nokia("99456-1234", "N95", "65465135156", 64);
+
+iphone.InstalarAplicativo("WhatsApp");
+iphone.Ligar();
+iphone.ReceberLigacao();
+
+nokia.InstalarAplicativo("Facebook");
+nokia.Ligar();
+iphone.ReceberLigacao();
+
+
